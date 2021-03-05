87ffa5c7e831e762cfe684260e172c2292defd27
Alan Lozano <alan.m@alimx.mx>
2021-03-05 17:33:27 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './components/Home';
+import Navbar from './components/common/Navbar';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/common/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const index = ({ }) => {
+    return (
+        <header>
+            <nav className="navbar navbar-light navbar-stargeek">
+                <a href="/" className="navbar-brand geek-logo">Geek</a>
+            </nav>
+        </header>
+    )
+}
+
+export default index;
